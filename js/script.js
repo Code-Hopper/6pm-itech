a2c6e94fd19b7f0473d7ad80e20975151e7c723d
code-hopper <ameykhondekar01@gmail.com>
2024-11-18 08:53:54 +0530

    javascript pratice day 1

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// data types Number, String, Boolean
+// how to create a variable(3 rules)
+// oprators(6 types of 7 oprators)
+
+// input and output statements/functions/methods
+// for user input window.prompt(string)
+
+// let number1 = 100;
+
+// let number2 = window.prompt("enter the value for number 2 !")
+
+// for output
+// window.alert(data)
+// let number = 123;
+// let name = "Amey Khondekar"
+// let age = 22
+
+// let name = window.prompt("enter your name")
+// let age = window.prompt("enter your age")
+
+// window.alert(name)
+
+// console.log(data)
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+// document.write(data)
+
+// document.write(name)
+// document.write(name)
+// document.write(name)
+// document.write(name)
+// document.write(name)
+// document.write(name)
+
+// document.write("hello !, my name is " + name + " and my age is " + age + " years.")
+
+// typeof() returns the datatype of passed args/data
+
+let data = true
+
+console.log(typeof(data))
+console.log(data)
+
+
+// find out what datatype window.prompt() data returns
